126f550869e3063986ba11b504b2eb27c85d1c66
Ebtihaj <85163024+Ibtihaj-Abdullah@users.noreply.github.com>
2024-10-31 15:17:28 +0500

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ echo "Hello World!<br>";
 
 // Initialize MySQL connection
 $con = mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, "./BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); // Full path to the certificate
+mysqli_ssl_set($con, NULL, NULL, "/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 
 // Connection credentials
 $server = "app1-server.mysql.database.azure.com"; // Replace with your server name
 $username = "kzuvscxtkl@app1-server"; // Replace with your username
-$password = "D5XB9kA2Ja\$VLoRw"; // Corrected password with escape character for special symbols
+$password = "D5XB9kA2Ja\$VLoRw"; // Make sure to escape any special characters in the password
 $database = "app1-database"; // Replace with your database name
 
 // Attempt to connect to the database
